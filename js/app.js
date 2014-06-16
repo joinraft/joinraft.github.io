778d0dcfae1cfea788689522bd92d02134c68e3f
Fredrik Johansson <fredrik.johansson@screeninteraction.com>
2014-06-16 22:27:19 +0200

    Added thank you after signup

--- js/app.js
+++ js/app.js
@@ -13,13 +13,13 @@ $('#signup_wrapper_thank_you').hide();
             'email' : $("#signup_field").val()
         }
 
-        postSuccess();
-        // $.post('http://fatcal.com/signup',params).done(function(data){
-        //     postSuccess();
-        // })
-        // .fail(function(data){
-        //     alert("Ooops, something went wrong. Please try again.");
-        // });
+        // postSuccess();
+        $.post('http://fatcal.com/signup',params).done(function(data){
+            postSuccess();
+        })
+        .fail(function(data){
+            alert("Ooops, something went wrong. Please try again.");
+        });
     });
 
     $("#blog").click(function()
@@ -35,23 +35,6 @@ $('#signup_wrapper_thank_you').hide();
 
 function postSuccess()
 {
-
   $('#signup_wrapper').hide();
   $('#signup_wrapper_thank_you').show();
-
-  // $("#signup_thankyou").fadeIn(800);
-
-
-  // $("#signup_field").html("Thank you!");
-  // $("#signup_btn")
-
-  // $('#email_btn').animate({opacity: 0}, 0, function()
-  // {
-  //     $(this).fadeOut(0);
-  // });
-
-  // $('#email_input').animate({opacity: 0}, 0, function()
-  // {
-  //     $(this).fadeOut(0);
-  // });
 }
